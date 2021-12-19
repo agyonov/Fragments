4448a0fc04c6ae1f85fe6585997d6cea82d11855
Aleksandar Gyonov <agyonov@hotmail.com>
2021-12-19 13:56:04 +0200

    first commit

---
+++ Fragments/FraApplication.cs
@@ -0,0 +1,27 @@
+﻿using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragments
+{
+    [Application(HardwareAccelerated = true)]
+    public class FraApplication : Application
+    {
+        public FraApplication(IntPtr handle, JniHandleOwnership transfer)
+            : base(handle, transfer)
+        {
+
+        }
+
+        public override void OnCreate()
+        {
+            // Call parent
+            base.OnCreate();
+
+            // Add here some init code
+        }
+    }
+}
---
+++ Fragments/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Fragments.FraApplication, Fragments, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64d08b0d92c0bc1521.FraApplication.class, crc64d08b0d92c0bc1521.FraApplication.__md_methods);
+		
+	}
+}
